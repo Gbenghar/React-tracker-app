67196f19c191aad2617ba3647e489e1203500ae2
Gbenga Olatunji <gbengaolatunji21@gmail.com>
2023-11-28 18:32:41 +0100

    Edit

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,54 +1,61 @@
-import React from "react";
-import { useState } from "react";
+import React from 'react'
+import { useState } from 'react'
 
 const AddTask = ({ onAdd, id }) => {
-  const [text, setText] = useState("");
-  const [day, setDay] = useState("");
-  const [reminder, setReminder] = useState(false);
-  const onSubmit = (event) => {
-    event.preventDefault();
+  const [text, setText] = useState('')
+  const [day, setDay] = useState('')
+  const [reminder, setReminder] = useState(false)
+  const onSubmit = event => {
+    event.preventDefault()
     if (!text) {
-      alert("Please add a task");
-      return;
+      alert('Please add a task')
+      return
     }
-    onAdd({ text, day, reminder });
-    setText("");
-    setDay("");
-    setReminder(false);
-  };
+    onAdd({ text, day, reminder })
+    setText('')
+    setDay('')
+    setReminder(false)
+  }
   return (
-    <form className="add-form" onSubmit={onSubmit}>
-      <div className="form-control">
+    <form className='add-form' onSubmit={onSubmit}>
+      <div className='form-control'>
         <label>Task</label>
         <input
-          type="text"
-          placeholder="Enter Task"
+          type='text'
+          placeholder='Enter Task'
           value={text}
-          onChange={(event) => setText(event.target.value)}
+          onChange={event => setText(event.target.value)}
         />
       </div>
-      <div className="form-control">
+      <div className='form-control'>
         <label>Day & Time</label>
         <input
-          type="text"
-          placeholder="Enter Day & Time"
+          type='text'
+          placeholder='Enter Day & Time'
           value={day}
-          onChange={(event) => setDay(event.target.value)}
+          onChange={event => setDay(event.target.value)}
         />
       </div>
-      <div className="form-control form-control-check">
+      <div className='form-control form-control-check'>
         <label>Set Reminder</label>
         <input
-          type="checkbox"
+          type='checkbox'
           checked={reminder}
           key={id}
           value={reminder}
-          onChange={(event) => setReminder(event.currentTarget.checked)}
+          onChange={event => setReminder(event.currentTarget.checked)}
         />
       </div>
-      <input type="submit" value="Save Task" className="btn btn-block" />
+      <label>
+        <input
+          key={id}
+          type='submit'
+          value='Save Task'
+          className='btn btn-block'
+        />
+      </label>
     </form>
-  );
-};
+  )
+}
 
-export default AddTask;
+export default AddTask
